ebbf253764535d510bd43d60506c6a91be89b27d
Budi Prasetyo <budi.prasetyo@turboly.com>
2019-11-05 18:03:59 +0700

    update

---
+++ db/migrate/20191105110247_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string 'title'
+      t.string 'author'
+      t.string 'type'
+      t.integer 'year'
+      t.timestamps
+    end
+  end
+end
